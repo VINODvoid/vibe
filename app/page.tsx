2aec25acc65bd41788c7ad09857f48aa77a4e681
VINODvoid <iamlearner.v@gmail.com>
2025-07-06 17:58:58 +0530

    feat: integrate Prisma for database management
    - Added Prisma as a dependency in package.json and package-lock.json.
    - Created a new Prisma schema file to define User and Post models.
    - Generated migration files for initial database setup, including User and Post tables with relationships.
    - Implemented a database connection in lib/db.ts using PrismaClient.
    - Updated the Home component to fetch and display users from the database.
    - Added generated Prisma client to .gitignore to prevent tracking of generated files.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import { Button } from "@/components/ui/button";
+import {prisma} from "@/lib/db";
 
 
-export default function Home() {
+export default async function Home() {
+  const users = await prisma.user.findMany();
+  
   return (
     <div>
       <Button variant={"default"}>click me
       </Button>
         <h1 className="font-bold">
-          hello
+          {JSON.stringify(users,null,2)}
           </h1>
     </div>
   );
 }
+  
